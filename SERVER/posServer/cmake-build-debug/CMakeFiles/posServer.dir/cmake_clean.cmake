26fb3ff32cee8fca18f6900516dd256709679a66
VeronyMark <128795527+VeronyMark@users.noreply.github.com>
2024-01-08 21:35:35 +0200

    Add files via upload

---
+++ SERVER/posServer/cmake-build-debug/CMakeFiles/posServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/posServer.dir/main.c.o"
+  "posServer"
+  "posServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/posServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
